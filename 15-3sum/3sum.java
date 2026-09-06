6d0d35ba972d4f384bc6d7f35cc43fcb15fd7df7
mohit24032005 <mohitag10105@gmail.com>
2026-09-06 18:59:02 +0530

    Time: 888 ms (7.33%) | Memory: 59.8 MB (18.96%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        HashSet<List<Integer>> res = new HashSet<>();
+        for(int i = 0; i<n-2; i++){
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                }else if(sum<0){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+        }
+        return new ArrayList<>(res);
+        
+    }
+}
